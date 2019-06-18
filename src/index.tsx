793f9ed1b0fbdbc88289c2fa65ac56cdd20aca9e
yukihane <yukihane.feather@gmail.com>
2019-06-18 13:22:16 +0900

    不要コード削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { DefaultTypelessProvider } from 'typeless';
 import CounterModule from './features/counter/module';
